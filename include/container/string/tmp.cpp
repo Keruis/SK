5a4ce80f78d0f3c58719fd0454cf721ee8889c82
Keruis <wuyutong0105@gmail.com>
2026-02-15 01:05:13 +0800

    init

--- include/container/string/tmp.cpp
+++ include/container/string/tmp.cpp
@@ -70,24 +70,14 @@ struct ct_find<N, SearchT, ct_t_array<Ts...>> {
         );
 };
 
-template<bool Cond, typename T, typename F>
-struct lazy_conditional {
-    using type = T;
-};
-
-template<typename T, typename F>
-struct lazy_conditional<false, T, F> {
-    using type = typename F::type;
-};
-
 template <std::size_t I, typename Key, typename... Entries>
 struct ct_find<I, Key, ct_table<Entries...>> {
     using entry = typename ct_table<Entries...>::template at<I>;
-    using type = lazy_conditional<
+    using type = std::conditional_t<
         std::is_same_v<Key, typename entry::key>,
         typename entry::value,
         ct_find<I + 1, Key, ct_table<Entries...>>
-    >::type;
+    >;
 };
 
 template <std::size_t, typename, std::size_t...>
@@ -98,45 +88,54 @@ struct ct_substr<P, ct_string<Cs...>, Is...> {
     using type = ct_string<Cs...[P + Is]...>;
 };
 
-template <char... Cs>
-struct ct_string {
-    using self = ct_string<Cs...>;
-
-    static constexpr std::size_t size = sizeof...(Cs);
-
-    static constexpr bool empty = size == 0;
-
-    template <std::size_t I>
-    static constexpr char at = Cs...[I];
-
-    template <std::size_t P, std::size_t C>
-    using substr = ct_substr<P, self, __integer_pack(C)...>::type;
-
-    template <typename Other>
-    using append = ct_concat<self, Other>::type;
-
-    template <typename Other>
-    static constexpr bool equal = 
-        std::is_same_v<
-            self,
-            Other
-        >;
-
-   template <typename Other, std::size_t S = 0>
-   static constexpr std::size_t find = ct_find<S, self, Other>::pos;
-
+template <auto& Arr, std::size_t... Is>
+struct ct_string_from_array {
+    using type = ct_string<Arr[Is]...>;
 };
 
-template <typename CT, CT... Cs>
-consteval auto operator""_cts() {
-    return std::type_identity<ct_string<Cs...>>{};
-}
+template <typename, typename, typename, std::size_t>
+struct ct_split;
 
-template <auto CTST>
-using cts = typename decltype(CTST)::type;
+template <typename Str, char... Cs, typename O>
+struct ct_split<Str, ct_string<Cs...>, O, ct_string<Cs...>::size> {
+    using type = O;
+};
+
+template <typename Str, char... Cs, std::size_t I, typename O>
+struct ct_split<Str, ct_string<Cs...>, O, I> {
+private:
+    using S = ct_string<Cs...>;
+    static constexpr std::size_t next_pos = S::template find<Str, I>;
+    static constexpr std::size_t token_len = (next_pos == static_cast<std::size_t>(-1)) ? S::size - I : next_pos - I;
+
+public:
+    using token = typename S::template substr<I, token_len>;
+    using type = typename ct_split<
+        Str,
+        S,
+        typename O::template add<token>,
+        (next_pos == static_cast<std::size_t>(-1) ? S::size : next_pos + Str::size)
+    >::type;
+};
 
 template <typename, typename, std::size_t>
 struct ct_remove_t;
+
+template <typename RemoveStr, char... Cs>
+struct ct_remove_t<RemoveStr, ct_string<Cs...>, static_cast<std::size_t>(-1)> {
+    using type = ct_string<Cs...>;
+};
+
+template <typename RemoveStr, char... Cs, std::size_t Pos>
+struct ct_remove_t<RemoveStr, ct_string<Cs...>, Pos> {
+    using type =
+        typename ct_string<Cs...>::template substr<0, Pos>::template append<
+            typename ct_string<Cs...>::template substr<
+                Pos + RemoveStr::size,
+                ct_string<Cs...>::size - Pos - RemoveStr::size
+            >
+        >;
+};
 
 template <typename A, typename... Ts>
 struct ct_remove_t<A, ct_t_array<Ts...>, ct_t_array<Ts...>::size> {
@@ -181,6 +180,72 @@ struct ct_at<ct_table<Entries...>, I> {
     using type = Entries...[I];
 };
 
+template <typename RemoveStr, typename Str, typename Erased = typename Str::template remove<RemoveStr>>
+struct ct_remove_all {
+    using type = typename ct_remove_all<RemoveStr, Erased>::type;
+};
+
+template <typename RemoveStr, typename Str>
+struct ct_remove_all<RemoveStr, Str, Str> {
+    using type = Str;
+};
+
+template <char... Cs>
+struct ct_string {
+    using self = ct_string<Cs...>;
+
+    static constexpr std::size_t size = sizeof...(Cs);
+
+    static constexpr bool empty = size == 0;
+
+    template <std::size_t I>
+    static constexpr char at = Cs...[I];
+
+    template <std::size_t P, std::size_t C>
+    using substr = ct_substr<P, self, __integer_pack(C)...>::type;
+
+    template <typename Other>
+    using append = ct_concat<self, Other>::type;
+
+    template <typename Other>
+    static constexpr bool equal = 
+        std::is_same_v<
+            self,
+            Other
+        >;
+
+    template <typename Other, std::size_t S = 0>
+    static constexpr std::size_t find = ct_find<S, self, Other>::pos;
+
+    template <const auto& Arr>
+    using from_array = ct_string_from_array<
+                        Arr, 
+                        __integer_pack(sizeof(Arr) - 1)...
+                    >::type;
+        
+    static consteval const char (&to_array())[size + 1] {
+        static constexpr char arr[size + 1] = { Cs..., '\0' };
+        return arr;
+    }
+
+    template <typename Str, typename O = ct_t_array<>, std::size_t S = 0>
+    using split = ct_split<Str, self, O, 0>::type;
+
+    template <typename RemoveStr>
+    using remove = ct_remove_t<RemoveStr, self, find<RemoveStr>>::type;
+
+    template <typename RemoveStr>
+    using remove_all = ct_remove_all<RemoveStr, self>::type;
+};
+
+template <typename CT, CT... Cs>
+consteval auto operator""_cts() {
+    return std::type_identity<ct_string<Cs...>>{};
+}
+
+template <auto CTST>
+using cts = typename decltype(CTST)::type;
+
 template <typename... Ts>
 struct ct_t_array {
     using self = ct_t_array<Ts...>;
@@ -222,8 +287,34 @@ struct ct_entry {
     using value = Value;
 };
 
+template <typename T>
+struct type_ {
+    using type = T;
+};
+
 template <typename... Entries>
 struct ct_table {
+    static_assert(
+        []{
+            if constexpr (sizeof...(Entries) < 2) {
+                return true;
+            } else {
+                constexpr std::size_t N = sizeof...(Entries);
+                return ([]<std::size_t... Is>() {
+                    return ([]<std::size_t I, std::size_t... Js>() {
+                        return ([]<std::size_t i, std::size_t j>() {
+                            if constexpr (j < i + 1) {
+                                return true;
+                            } else {
+                                return !std::is_same_v<typename Entries...[i]::key, typename Entries...[j]::key>;
+                            }
+                        }.template operator()<I, Js>() && ...);
+                    }.template operator()<Is, __integer_pack(N)...>() && ...);
+                }.template operator()<__integer_pack(N - 1)...>());
+            }
+        }()
+    );
+
     using self = ct_table<Entries...>;
 
     static constexpr std::size_t size = sizeof...(Entries);
@@ -247,7 +338,34 @@ struct ct_table {
 
     template <typename Key, std::size_t S = 0>
     using get = ct_find<S, Key, self>::type;
+
+    template <typename Key, typename Default, std::size_t S = 0>
+    using get_or = std::conditional_t<
+        contains<Key>,
+        ct_find<S, Key, self>,
+        std::type_identity<Default>
+    >::type;
 };
+
+#define STRING_FLAG()
+
+namespace string_flag {
+    struct None{};
+    struct Mutable{};
+    struct Shared{};
+    struct Noexcept{};
+    struct Backtracking{};
+};
+
+
+template <typename Policies>
+struct string_context {
+    using self = string_context<Policies>;
+
+    using get_policies = Policies;
+};
+
+
 
 }
 
@@ -262,14 +380,26 @@ struct print_t {
     }
 };
 
+consteval bool arr_equal(const char* a, const char* b, std::size_t N) {
+    for (std::size_t i = 0; i < N; ++i) {
+        if (a[i] != b[i]) return false;
+    }
+    return true;
+}
+
 int main() {
     using A = cts<"abcd"_cts>;
     using B = cts<"ab"_cts>;
+
+    static constexpr char test[] = "hello";
 
     static_assert(A::equal<B::append<cts<"cd"_cts>>>);
     static_assert(A::substr<1, 2>::at<0> == 'b');
     static_assert(A::size == 4);
     static_assert(A::find<cts<"cd"_cts>> == 2);
+    static_assert(std::is_same_v<ct_string<>::from_array<test>, cts<"hello"_cts>>);
+    static_assert(arr_equal(cts<"hello"_cts>::to_array(), test, 6));
+    static_assert(std::is_same_v<A::remove<cts<"ab"_cts>>, cts<"cd"_cts>>);
 
     using C = ct_t_array<int, double>;
     static_assert(std::is_same_v<C::at<0>, int>);
@@ -295,8 +425,60 @@ int main() {
 
     static_assert(D::size == 2);
     static_assert(std::is_same_v<D::get<int>, char>);
+    static_assert(std::is_same_v<D::get_or<int, void>, char>);
+    static_assert(std::is_same_v<D::get_or<char, void>, void>);
     static_assert(std::is_same_v<D::remove<int>, ct_table<
         ct_entry<double, bool>
     >>);
     static_assert(D::has<int>);
+
+    using E = string_context<
+        ct_table<
+            ct_entry<
+                cts<"test1"_cts>, 
+                ct_t_array<
+                    string_flag::Mutable,
+                    string_flag::Noexcept
+                >
+            >,
+            ct_entry<
+                cts<"test2"_cts>,
+                ct_t_array<
+                    string_flag::Shared, 
+                    string_flag::Backtracking
+                >
+            >
+        >
+    >;
+
+    static_assert(
+        std::is_same_v<E::get_policies::get_or<cts<"test1"_cts>, void>::at<0>, string_flag::Mutable>
+    );
+
+
+
+    static constexpr char text[] = R"(
+        hello_1
+        hello_2
+        hello_3
+    )";
+
+    using T  = ct_string<>::from_array<text>;
+    using A_ = T::split<cts<"\n"_cts>>;
+
+    static_assert(std::is_same_v<
+        A_::at<1>::remove_all<cts<" "_cts>>,
+        cts<"hello_1"_cts>
+    >);
+
+    static_assert(std::is_same_v<
+        A_::at<2>::remove_all<cts<" "_cts>>,
+        cts<"hello_2"_cts>
+    >);
+
+    static_assert(std::is_same_v<
+        A_::at<3>::remove_all<cts<" "_cts>>,
+        cts<"hello_3"_cts>
+    >);
+
 }
